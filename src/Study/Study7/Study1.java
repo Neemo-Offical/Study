0362b02c1f95a803e0126ddd070754492f5b8535
Neemo-Offical <y153426y@163.com>
2021-06-21 08:33:55 +0800

    精简了一下

--- src/Study/Study7/Study1.java
+++ src/Study/Study7/Study1.java
@@ -1,4 +1,27 @@
 package src.Study.Study7;
+import java.util.Scanner;
 
 public class Study1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of students:");
+        int members = input.nextInt();
+        int[] score = new int[members];
+        System.out.println("Enter " + score.length + " scores:");
+        for (int i = 0; i < score.length; i++) {
+            score[i] = input.nextInt();
+        }
+        int best = score[0];
+        for (int i=0; i < score.length; i++) {
+            if (score[i] > best) best = score[i];
+            }
+        for (int i = 0; i < score.length; i++) {
+            String grade = "F";
+            if (score[i] >= best - 10) grade = "A";
+            else if (score[i] >= best - 20) grade = "B";
+            else if (score[i] >= best - 30) grade = "C";
+            else if (score[i] >= best - 40) grade = "D";
+            System.out.println("Student " + i + " score is " + score[i] +" and grade is " + grade);
+        }
+    }
 }
